7fe03d9ab2bbba08c1d7265615a881bc88d0d758
Fayed Raza <42160652+fayedraza@users.noreply.github.com>
2020-07-07 00:42:54 -0400

    Update webCrawler.py

--- BuildingAWebCrawler/webCrawler.py
+++ BuildingAWebCrawler/webCrawler.py
@@ -1,14 +1,15 @@
 import requests 
 from bs4 import BeautifulSoup 
 
-
 def item_spider(max_pages):
     page = 1
+    
     while page <= max_pages:
         url = 'https://www.ebay.com/e/_electronics/best-of-tech-deals/cell-phones-accessories/15032?_pgn=' + str(page)      
         source_code = requests.get(url) 
-        plain_text = source_code.text 
-        soup = BeautifulSoup([plain_text], "html.parser")  
+        text = source_code.text
+        soup = BeautifulSoup(text,features="lxml")  
+
         for link in soup.findAll('a', {'class': 's-item__title'}): 
             h3 = "https://www.ebay.com" + link.get('h3')
             title = link.string 
@@ -18,4 +19,4 @@ def item_spider(max_pages):
 
 
 
-item_spider(1) 
+item_spider(2) 
